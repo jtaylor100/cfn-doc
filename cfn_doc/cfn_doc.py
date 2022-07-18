faa1e630a6abf7fa5004f81f65d7e094c53ce1d6
Joshua Taylor <joshua.taylor@thoughtworks.com>
2022-07-18 14:31:07 +0100

    Generate for many templates at once

--- cfn_doc/cfn_doc.py
+++ cfn_doc/cfn_doc.py
@@ -10,12 +10,13 @@ def main(argv):
     parser.add_argument("template_files", nargs="+", metavar="template-file")
     args = parser.parse_args(argv[1:])
 
-    template_file_path = Path(args.template_files[0])
-    with open(template_file_path, "r") as f:
-        template_yaml = yaml.load(f, Loader=yaml.SafeLoader)
+    for template_file_path_str in args.template_files:
+        template_file_path = Path(template_file_path_str)
+        with open(template_file_path, "r") as f:
+            template_yaml = yaml.load(f, Loader=yaml.SafeLoader)
 
-    template_file_name = template_file_path.name
-    template_doc_file_name = template_file_name.replace(".yaml", "-doc.md")
-    template_doc_file_path = template_file_path.parent / template_doc_file_name
-    with open(template_doc_file_path, "w+") as f:
-        f.write("# " + template_yaml["Description"])
+        template_file_name = template_file_path.name
+        template_doc_file_name = template_file_name.replace(".yaml", "-doc.md")
+        template_doc_file_path = template_file_path.parent / template_doc_file_name
+        with open(template_doc_file_path, "w+") as f:
+            f.write("# " + template_yaml["Description"])
--- test/test_cfn_doc.py
+++ test/test_cfn_doc.py
@@ -9,37 +9,62 @@ from cfn_doc.cfn_doc import main
 
 class MainTest(unittest.TestCase):
     def test_it_generates_document_file(self):
-        temp_dir = tempfile.TemporaryDirectory()
-        test_dir = Path(__file__).parent
-        prime_test_template_path = str((test_dir / "basic-template.yaml").resolve())
-        clone_test_template_path = str(
-            (Path(temp_dir.name) / "basic-template.yaml").resolve()
-        )
-        shutil.copyfile(prime_test_template_path, clone_test_template_path)
+        test_template_path = self.copy_fixture_to_temp_dir("basic-template.yaml")
 
-        main(["cfn-doc", clone_test_template_path])
+        main(["cfn-doc", test_template_path])
 
-        expected_doc_path = (Path(temp_dir.name) / "basic-template-doc.md").resolve()
-        self.assertTrue(
-            os.path.exists(expected_doc_path),
-            f"Generated document file not found at {str(expected_doc_path)}",
+        self.assertPathExists(self.applyTempPath("basic-template-doc.md"))
+
+    def test_it_generates_multiple_document_files(self):
+        test_template_path = self.copy_fixture_to_temp_dir(
+            "basic-template.yaml", "basic-template.yaml"
         )
-        temp_dir.cleanup()
+        second_test_template_path = self.copy_fixture_to_temp_dir(
+            "basic-template.yaml", "basic-template-second.yaml"
+        )
+
+        main(["cfn-doc", test_template_path, second_test_template_path])
+
+        self.assertPathExists(self.applyTempPath("basic-template-doc.md"))
+        self.assertPathExists(self.applyTempPath("basic-template-second-doc.md"))
 
     def test_it_outputs_template_title(self):
-        temp_dir = tempfile.TemporaryDirectory()
-        test_dir = Path(__file__).parent
-        prime_test_template_path = str((test_dir / "basic-template.yaml").resolve())
-        clone_test_template_path = str(
-            (Path(temp_dir.name) / "basic-template.yaml").resolve()
+        test_template_path = self.copy_fixture_to_temp_dir(
+            "basic-template.yaml", "basic-template.yaml"
         )
-        shutil.copyfile(prime_test_template_path, clone_test_template_path)
 
-        main(["cfn-doc", clone_test_template_path])
+        main(["cfn-doc", test_template_path])
 
-        expected_doc_path = (Path(temp_dir.name) / "basic-template-doc.md").resolve()
+        expected_doc_path = (
+            Path(self.temp_dir.name) / "basic-template-doc.md"
+        ).resolve()
         with (open(expected_doc_path, "r+")) as f:
             actual_doc = f.read()
 
         self.assertRegex(actual_doc, "# A sample template")
-        temp_dir.cleanup()
+
+    def copy_fixture_to_temp_dir(self, fixture_path, dest_name=None):
+        if dest_name is None:
+            dest_name = fixture_path
+
+        test_dir = Path(__file__).parent
+        prime_test_template_path = str((test_dir / fixture_path).resolve())
+        clone_test_template_path = str((Path(self.temp_dir.name) / dest_name).resolve())
+        shutil.copyfile(prime_test_template_path, clone_test_template_path)
+        return clone_test_template_path
+
+    def assertPathExists(self, path):
+        self.assertTrue(
+            os.path.exists(path),
+            f"File not found at {str(path)}",
+        )
+
+    def applyTempPath(self, file_name):
+        return (Path(self.temp_dir.name) / file_name).resolve()
+
+    def setUp(self):
+        self.temp_dir = tempfile.TemporaryDirectory()
+
+    def tearDown(self):
+        self.temp_dir.cleanup()
+        del self.temp_dir
